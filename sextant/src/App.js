8c7b3592b06b350eaf65c55d33d72ae887abe88c
Ashik Manikandan Chandrasekaran <scauldron50@gmail.com>
2022-08-11 15:30:52 +0530

    Added banner and card

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+        <Banner/>
+        <Exhibit/>
     </div>
+    );
+}
+function Banner() {
+  return (
+    <div class="banner">
+        <div class="banner__content">
+            <div class="banner__text">
+                <strong><p>Project Sextant</p></strong>
+            </div>
+        </div>
+    </div>
+    
   );
 }
+function Exhibit() {
+    return (
+    <div class="exhibit">
+        <div class="heading">
+            <h3>Wrapper</h3>
+        </div>
+    </div>
+    );
+  }
 
 export default App;
